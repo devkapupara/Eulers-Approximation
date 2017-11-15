02c0e2ae0e32f8ce4414aa4799041eceab2a55d3
itchiTriggah <itchiTriggah@gmail.com>
2017-11-14 22:21:02 -0800

    First Commit

---
+++ EulersEstimation.py
@@ -0,0 +1,46 @@
+import parser, math
+
+def parseEquation(exp):
+    return parser.expr(exp).compile()
+
+def EulersEquation(eq,xVal,y,h,n):
+    results = []
+    for i in range(n):
+        x = xVal.pop(0)
+        y = y + h*eval(eq)
+        results.append((x,y))
+    return results
+
+def realFunction(re,h):
+    r = []
+    e = math.exp(1)
+    for x in h:
+        r.append(eval(re))
+    return r
+
+def printResults(ans,h):
+    for i in range(len(ans)):
+        print("(x{:.1f}, y{:.1f}) = ({:.4f}, {:.4f})".format((i+1)*h,(i+1)*h,ans[i][0],ans[i][1]))
+
+def printError(err,h):
+    for i in range(len(h)):
+        print("y{:.2f} = {:.4f}".format(h[i],err[i]))
+
+def error(real,euler,n):
+    return [abs(real[i]-euler[i][1]) for i in range(n)]
+
+if __name__ == "__main__":
+    exp = parseEquation(input("Enter f(x) = "))
+    y = float(input("Enter y0: "))
+    x = float(input("Enter x0: "))
+    h = float(input("Enter step size: "))
+    n = int(input("How many values of y to calculate?: "))
+    hVal = [i * h for i in range(1, n+1)]
+    xVal = [x + i*h for i in range(n)]
+    eulerResult = EulersEquation(exp,xVal,y,h,n)
+    printResults(eulerResult,h)
+    re = parseEquation(input("Enter Analytic Function: "))
+    realResult = realFunction(re,hVal)
+    err = error(realResult,eulerResult,n)
+    print("Error:")
+    printError(err,hVal)
